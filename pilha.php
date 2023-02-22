ab481a4eeb51a3a0093c09c856a17bb4b3af0a25
DanJettax <daniel@jettax.com.br>
2023-02-22 08:52:01 -0300

    [daniel-Alura] - commit aula 2 exception

--- pilha.php
+++ pilha.php
@@ -3,16 +3,31 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+    try {
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError  $erro) {
+        echo "Na função 1 resolvi o problema da função 2 " . PHP_EOL;
+        echo $erro->getMessage().PHP_EOL;
+        echo $erro->getLine().PHP_EOL;
+        echo $erro->getTraceAsString().PHP_EOL;
+
+}
+
+
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $divisao = intdiv(5,0);
+    $arrayFixo = new SplFixedArray(2);  //Uncaught RuntimeException
+    $arrayFixo[3] = 'valor';
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
+
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
